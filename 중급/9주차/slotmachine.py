82a60a00a505a48be39b72d8c6ee1c6f6e283cbe
dkeif <hjhyeon0514@gmail.com>
2022-01-22 18:30:31 +0900

    z

--- 중급/9주차/slotmachine.py
+++ 중급/9주차/slotmachine.py
@@ -7,21 +7,21 @@ def RandomNumber():
     return num1, num2, num3
 
 def SlotMachine():
+    num1, num2, num3 = RandomNumber()
     coin = input('<Slot Machine>\n코인을 넣어주세요! 1.코인넣기//2.그만히기>>')
     try:
         user_answer2 = int(coin)
     except ValueError:
         print('잘못 입력하셨습니다')
-    if main == 1:
-        print("-------------------\n|"%c"%chr(num1)"|"%c"%chr(num2)"%c"%chr(num3)"")
+    if int(coin) == 1:
+        print("%c"%chr(num1))
 
 print('[SlotMachine GAME]')
 main = input('1.게임시작//2.나가기>>')
 
 try:
     user_answer = int(main)
-except ValueError:
+except ValueError as e:
     print('잘못 입력하셨습니다')
-
 if int(main) == 1:
-    SlotMachine()
+    print('와')
